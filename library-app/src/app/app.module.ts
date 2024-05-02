7a260c68acb6f536130778b23f167ccec508c2db
brassare <brassare@hotmail.com>
2024-05-02 11:25:06 -0300

    * Creating  /core/components/material.module.ts for export usign material
    *Creating  /core/core.module.ts for import and export material.modules.ts
    
    * in app.module.ts import core.module

--- library-app/src/app/app.module.ts
+++ library-app/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { CoreModulesTsModule } from './core/components/core.modules';
+import { CoreModulesTsModule } from './core/components/material.modules';
 
 
 
